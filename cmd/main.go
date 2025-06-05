f0afc2484b458b3b4c3ce0d429b3fd3fb51e55e0
REBORNOFF <mailabc.ru@yandex.ru>
2025-06-06 01:46:24 +0300

    feat: Created a server and added the first NewAuthHandler

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"app/breafURL/configs"
+	"app/breafURL/internal/auth"
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	config := configs.LoadConfig()
+	router := http.NewServeMux()
+
+	auth.NewAuthHandler(router, auth.AuthHandlerDeps{
+		Config: config,
+	})
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	fmt.Println("Server started on 8080 port")
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 }
