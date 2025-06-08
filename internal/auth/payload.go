e4d0fb176dda968157322ecd5f6b9af82a2c305a
REBORNOFF <mailabc.ru@yandex.ru>
2025-06-08 11:50:31 +0300

    feat: Added payload structures for response and request

--- internal/auth/payload.go
+++ internal/auth/payload.go
@@ -5,6 +5,16 @@ type LoginResponse struct {
 }
 
 type LoginRequest struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required"`
+}
+
+type RegisterRequest struct {
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required"`
+	Name     string `json:"name" validate:"required"`
+}
+
+type RegisterResponse struct {
+	Token string `json:"token"`
 }
